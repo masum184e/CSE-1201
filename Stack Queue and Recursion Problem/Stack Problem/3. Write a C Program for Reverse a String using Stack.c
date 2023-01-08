d7e1265c04c6399a5b1d7bc3357e873f7ae87329
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-01-08 23:05:54 +0600

    Initial Commit

---
+++ Stack Queue and Recursion Problem/Stack Problem/3. Write a C Program for Reverse a String using Stack.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+#define STACK_MAX 100
+typedef struct stack Stack;
+struct stack{
+  int top;
+  char data[STACK_MAX];
+};
+int pop(Stack *s){
+  int item;
+  if(s->top==0){
+    printf("Stack is Empty");
+    return -1;
+  }else{
+    s->top=s->top-1;
+    item=s->data[s->top];
+  }
+  return item;
+}
+void push(Stack *s, int item){
+  if(s->top<STACK_MAX){
+    s->data[s->top]=item;
+    s->top=s->top+1;
+  }else{
+    printf("Stack Is Full");
+  }
+}
+int main(){
+  Stack my_stack;
+  my_stack.top=0;
+
+  char str[STACK_MAX];
+  printf("Enter A String: ");
+  scanf("%s",&str);
+  
+  for(int i=0;i<strlen(str);i++)push(&my_stack,str[i]);
+
+  for(int i=0;i<strlen(str);i++)str[i]=pop(&my_stack);
+
+  printf("Reverse String: %s",str);
+
+  return 0;
+}
