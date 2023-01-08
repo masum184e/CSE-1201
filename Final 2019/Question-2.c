8cec955e1369d7169d9fc48d1d46604f6c31eea5
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-01-09 00:14:21 +0600

    Initial Commit

---
+++ Final 2019/Question-2.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#define STACK_MAX 5
+typedef struct stack Stack;
+struct stack{
+  int top;
+  int data[STACK_MAX];
+};
+int pop(Stack *s){
+  int item;
+  if(s->top==0){
+    printf("Stack is Empty");
+    return -1;
+  }else{
+    s->top=s->top-1;
+    item=s->data[s->top];
+  }
+  return item;
+}
+void push(Stack *s, int item){
+  if(s->top<STACK_MAX){
+    s->data[s->top]=item;
+    s->top=s->top+1;
+  }else{
+    printf("Stack Is Full");
+  }
+}
+int main(){
+  Stack my_stack;
+  my_stack.top=0;
+
+  push(&my_stack,1);
+  printf("%d\n",pop(&my_stack));
+  push(&my_stack,2);
+  push(&my_stack,3);
+  push(&my_stack,4);
+  printf("%d\n",pop(&my_stack));
+  printf("%d\n",pop(&my_stack));
+  push(&my_stack,5);
+
+  return 0;
+}
